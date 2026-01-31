5fe06ad44c58bcedca8c4c3a79cad0030edeb06f
Mert Rasna <mrasnalu02@gmail.com>
2026-01-31 16:44:58 +0100

    settings file for constant variables and first game screen

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print("Nogame the game :)")
+# Pygame works with pixels, not math-based vectors
+import pygame
+import sys 
+from src.settings import * # module import(global) for speed, from src import settigns later
+
+def main():
+
+    pygame.init()
+
+    # Creating the window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Nogame")
+
+    clock = pygame.time.Clock()
+
+    # Temporary "Rect" representing our player
+    player_rect = pygame.Rect(100, FLOOR_Y - 100, 50, 100)
+
+    # -- THE GAME LOOP --
+    running = True
+    while running:
+        # Check for events 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        # Draw everything
+        screen.fill(BLACK) # Clear screen first
+
+        # Temp player
+        pygame.draw.rect(screen, RED, player_rect)
+
+        # Update the display
+        pygame.display.flip()
+
+        # Keep the game running at exactly 60 FPS
+        clock.tick(FPS)
+
+    pygame.quit() # shut downs the game engine
+    sys.exit() # tells this python program is finished, close it and give memory back 
+
+
+# Safety switch
+if __name__ == "__main__":
+    main()    
---
+++ src/settings.py
@@ -0,0 +1,18 @@
+# Our "rules" file for defining constants
+
+# Screen Configuration
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+FPS = 60
+
+# Physics constants
+"""
+Pygame starts (0,0) at the top-left corner and as you go down, the Y value increases.
+"""
+GRAVITY = 0.8
+FLOOR_Y = 600 # The pixel height where the ground is. Like a drawing an invisible line 
+
+# Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
