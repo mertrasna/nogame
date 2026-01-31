b3e3acd983b8c5230d0ae67150eb2d267bec8c42
Mert Rasna <mrasnalu02@gmail.com>
2026-01-31 23:05:27 +0100

    feat: fullscreen support and quit button

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
 
     pygame.init()
     # Creating the window
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
     pygame.display.set_caption("Nogame")
     clock = pygame.time.Clock()
 
@@ -19,17 +19,24 @@ def main():
 
     # -- THE GAME LOOP --
     running = True
+    # Background configs
+    bg_image = pygame.image.load("assets/backgrounds/game_background_1.png").convert() 
+    bg_image = pygame.transform.scale(bg_image,(SCREEN_WIDTH, SCREEN_HEIGHT))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
 
         # --- THE HEARTBEAT ---
         # This runs move() and apply_physics() 60 times a second
         p1.update()
         p2.update()
-        # --- THE CANVAS ---
-        screen.fill(BLACK) # Clear the old frame
+        # drawing the background at (0,0) - the top left corner
+        screen.blit(bg_image, (0,0))
         
         # Draw the character at their new position
         p1.draw(screen)
